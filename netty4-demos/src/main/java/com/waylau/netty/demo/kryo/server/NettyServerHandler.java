db540ef8c162d8535dc8575a476c451bf0fe9caf
zhankun <zhankun@egova.com.cn>
2024-08-05 13:58:20 +0800

    netty中kryo序列化

---
+++ netty4-demos/src/main/java/com/waylau/netty/demo/kryo/server/NettyServerHandler.java
@@ -0,0 +1,43 @@
+package com.waylau.netty.demo.kryo.server;
+
+
+import com.waylau.netty.demo.kryo.dto.RpcRequest;
+import com.waylau.netty.demo.kryo.dto.RpcResponse;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import lombok.extern.slf4j.Slf4j;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author shuang.kou
+ * @createTime 2020年05月13日 20:44:00
+ */
+@Slf4j
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final AtomicInteger atomicInteger = new AtomicInteger(1);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            RpcRequest rpcRequest = (RpcRequest) msg;
+            log.info("server receive msg: [{}] ,times:[{}]", rpcRequest, atomicInteger.getAndIncrement());
+            RpcResponse messageFromServer = RpcResponse.builder().message("message from server").build();
+            ChannelFuture f = ctx.writeAndFlush(messageFromServer);
+            f.addListener(ChannelFutureListener.CLOSE);
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error("server catch exception", cause);
+        ctx.close();
+    }
+}
